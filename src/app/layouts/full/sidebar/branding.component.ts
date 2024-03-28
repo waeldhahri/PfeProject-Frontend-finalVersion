b230b6ca8d3557fa44b85a78cec77dd20d537e24
waelepi <waeldhahri0@gmail.com>
2024-03-28 02:24:59 +0100

    first commit

---
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branding',
+  template: `
+    <div class="branding">
+      <a href="/">
+        <img
+          src="./assets/images/favicon.png"
+          class="align-middle m-2"
+          alt="logo"
+        />
+
+        <img
+          src="./assets/images/Leoni.png"
+          class="align-middle m-2 " style="width: 130px; height: auto"
+          alt="logo"
+        />
+      </a>
+    </div>
+  `,
+})
+export class BrandingComponent {
+  constructor() {}
+}
